9b9759643972caaac5624a23e4719c7a5e8cef95
gracekells <graceksd16@gmail.com>
2022-10-12 17:02:39 +0700

    update implicit

--- app/src/main/java/com/si5a/implicitsi5a/MainActivity.java
+++ app/src/main/java/com/si5a/implicitsi5a/MainActivity.java
@@ -1,7 +1,11 @@
 package com.si5a.implicitsi5a;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -17,9 +21,41 @@ public class MainActivity extends AppCompatActivity {
         etUrl = findViewById(R.id.et_url);
         etLokasi = findViewById(R.id.et_lokasi);
         etTeks = findViewById(R.id.et_teks);
+
         btnBukaWebsite = findViewById(R.id.btn_buka_website);
         btnBukaLokasi = findViewById(R.id.btn_buka_lokasi);
         btnBagikanTeks = findViewById(R.id.btn_bagikan_teks);
+
+        btnBukaWebsite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String urlWebsite = etUrl.getText().toString();
+                Uri uriUrlWebsite = Uri.parse(urlWebsite);
+                Intent intentWebsite = new Intent(Intent.ACTION_VIEW, uriUrlWebsite);
+                try {
+                    startActivity(intentWebsite);
+                } catch (Exception e){
+                    etUrl.setError("URL Tidak Sesuai!");
+                }
+
+
+            }
+        });
+
+        btnBukaLokasi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+            }
+        });
+
+        btnBagikanTeks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
 
     }
 }
